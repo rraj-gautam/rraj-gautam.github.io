a0778b6c296638ce390475617fcccf39d50fec22
rishi-takepart <rishi.gautam@takepart.ch>
2022-12-20 11:46:26 +0545

    added copy button

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,7 +134,20 @@ for (let i = 0; i < formInputs.length; i++) {
   });
 }
 
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
 
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
